9ea45977cd4f71a115f1bcc97bb9c4b636da0e0d
phucvinh57 <npvinh0507@gmail.com>
2023-01-18 08:43:59 +0700

    complete sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
-import { FaAccessibleIcon, FaDatabase } from "react-icons/fa";
+import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent } from "react-pro-sidebar";
+import { Button } from "react-bootstrap";
+import { FaPrint, FaDatabase, FaBars, FaGithub } from "react-icons/fa";
+import { useState } from "react";
+import { SidebarWidth } from "./constants";
+
 export default function App() {
+  const [sidebarCollapse, setSidebarCollapse] = useState(true);
+
   return (
-    <ProSidebar>
-      <Menu iconShape='square'>
-        <MenuItem icon={<FaDatabase />}>Dashboard</MenuItem>
-        <SubMenu title='Components' icon={<FaAccessibleIcon />}>
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
+    <div>
+      <ProSidebar style={{ position: "fixed", height: "100vh" }} collapsed={sidebarCollapse}>
+        <SidebarHeader>
+          <Menu iconShape='round'>
+            <MenuItem
+              icon={<FaBars onClick={() => setSidebarCollapse(!sidebarCollapse)} />}
+              style={{ fontSize: "18px", fontWeight: "bold" }}
+            >
+              Chùa Hội Khánh
+            </MenuItem>
+          </Menu>
+        </SidebarHeader>
+        <SidebarContent>
+          <Menu iconShape='circle'>
+            <MenuItem icon={<FaDatabase />}>Danh sách phật tử</MenuItem>
+            <MenuItem icon={<FaPrint />}>In ấn</MenuItem>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <Menu iconShape='circle'>
+            <MenuItem icon={<FaGithub />}>Liên hệ</MenuItem>
+          </Menu>
+        </SidebarFooter>
+      </ProSidebar>
+      <div id='main' style={{ marginLeft: `${sidebarCollapse ? SidebarWidth.COLLAPSE : SidebarWidth.EXPAND}px` }}>
+        <Button>Toggle</Button>
+      </div>
+    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./scss/index.scss";
+import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
